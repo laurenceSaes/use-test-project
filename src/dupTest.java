10fc78807e7cc75c7a0ce819a6566916013e75ae
Laurence Saes <l.saes@live.nl>
2017-12-18 16:06:06 +0100

    New test

---
+++ src/dupTest.java
@@ -0,0 +1,28 @@
+import javax.annotation.Generated;
+
+class dupTest {
+	public void testM() {			//Comment
+		int i1 = 1 + 1;				//Comment
+		int i2 = 1 + 1 * 2;			//Comment
+/*test*/int i3 = 1 + 1 / 4;/*test*/
+}
+	
+	public void testM2() {			//Some test comment
+/*test*/int i1 = 1 + 1;
+		int i2 = 1 + 1 * 2;/*test*/
+		/*
+		 * test
+		 */
+		int i3 = 1 + 1 / 4;
+	}
+	
+	public void testM3() {
+		int i2 = 1 /*test*/+ 1 * 2;
+		int i3 = 1 + 1 / 4;
+		/*
+		int i1 = 1 + 1;
+		int i2 = 1 + 1 * 2;
+		*/
+	}
+	
+}
